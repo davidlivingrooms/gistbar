dcd272a1e7d27fdaf4b27afdd8053ae8087a8271
david <davidlivingrooms@gmail.com>
2017-06-17 21:54:37 -0600

    Adding build step and dependencies

---
+++ index.js
@@ -0,0 +1,50 @@
+const { app, ipcMain, globalShortcut, Menu } = require('electron')
+const menubar = require('menubar')
+
+const mb = menubar({
+  // dir: path.join(__dirname, '/app'),
+  width: 1000,
+  height: 800,
+  // icon: path.join(__dirname, '//app/Icon-Template.png'),
+  preloadWindow: true,
+  windowPosition: 'topRight',
+})
+
+const template = [
+  {
+    label: 'Gistbar',
+    submenu: [
+      {
+        label: 'Quit App',
+        accelerator: 'CommandOrControl+Q',
+        selector: 'terminate:'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator: 'Alt+CommandOrControl+I',
+        click: function () { mb.window.toggleDevTools() }
+      }
+    ]
+  }
+]
+
+mb.on('ready', function ready () {
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
+  // register the global wakeup shortcut
+  globalShortcut.register('CommandOrControl+Shift+Space', () => {
+
+    if (mb.window.isVisible()) {
+      return mb.hideWindow()
+    }
+
+    mb.showWindow()
+    mb.window.focus()
+  })
+})
+
+mb.app.on('will-quit', function () {
+  globalShortcut.unregisterAll()
+})
+
