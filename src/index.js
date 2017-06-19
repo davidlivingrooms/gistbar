70ccbf062668ae4e692ce18c7cd84f18714d1609
david <davidlivingrooms@gmail.com>
2017-06-18 22:16:36 -0600

    adding material-ui to dependencies. working on main page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MenuApp from './components/MenuApp'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+injectTapEventPlugin() // Needed for material-ui. See https://github.com/callemall/material-ui#react-tap-event-plugin
+
+const App = () => (
+  <MuiThemeProvider>
+    <MenuApp />
+  </MuiThemeProvider>
+)
 
 ReactDOM.render(
-  <MenuApp/>,
+  <App/>,
   document.getElementById('root')
 )
